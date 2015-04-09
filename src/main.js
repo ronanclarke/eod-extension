0b3b2df0e6b9a708871a65d89b7d844384afdabd
Ronan Clarke <ronanclarke@gmail.com>
2015-04-09 12:02:27 +0100

    improved login error handling and link parsing

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ function renderStatus(statusText) {
 function buildHTMLfromViews(views) {
 
     renderStatus("");
-    
+
     if (views.length < 1) {
         renderStatus("no EoD instances found");
         return;
@@ -37,14 +37,25 @@ function buildHTMLfromViews(views) {
             var label = view.url.replace("https://jenkins.whatclinic.net/view/ENV%20", "").replace("/", "");
 
             sHTML += "<tr>";
-            sHTML += '<td class="instance-name"><a href="http://no-cache.' + label + '.varnish.whatclinic.net" target="_blank">' + label + "</a></td>"
-            sHTML += '<td class="jenkins-tab"><a href="' + view.url + '" target="_blank"><image src="jenkins.png"/></a></td>';
-
+            sHTML += '<td class="instance-name"><a href="http://' + label + '.eod.whatclinic.net" target="_blank">' + label + "</a></td>"
+            sHTML += '<td class="jenkins-tab"><a href="' + view.url + '" target="_blank"><image alt="' + label + '" src="jenkins.png"/></a></td>';
             sHTML += "</tr>"
         }
 
-
     });
+
+    sHTML += "<tr><td colspan='2'>----------------------------</td> ";
+
+    sHTML += "<tr>";
+    sHTML += '<td class="instance-name"><a href="http://no-cache.en.staging.varnish.whatclinic.com" target="_blank">Staging</a></td>'
+    sHTML += '<td class="jenkins-tab"><a href="https://eng.whatclinic.com/jenkins/view/Eng%20Staging/" target="_blank"><image src="jenkins.png"/></a></td>';
+    sHTML += "</tr>"
+
+    sHTML += "<tr>";
+    sHTML += '<td class="instance-name"><a href="http://no-cache.en.dev.varnish.whatclinic.com" target="_blank">Dev</a></td>'
+    sHTML += '<td class="jenkins-tab"><a href="https://eng.whatclinic.com/jenkins/view/Eng%20Dev/" target="_blank"><image src="jenkins.png"/></a></td>';
+    sHTML += "</tr>"
+
 
     sHTML += "</tbody></table>";
 
@@ -54,28 +65,38 @@ function buildHTMLfromViews(views) {
 
 function getInstancesFromJenkins() {
     var searchUrl = 'https://jenkins.whatclinic.net/api/json?pretty=true';
+    var timer = setTimeout(function () {
+        x.abort();
+        renderLogin();
+    }, 4000);
     console.log("hitting " + searchUrl);
     var x = new XMLHttpRequest();
     x.open('GET', searchUrl);
     // The Google image search API responds with JSON, so let Chrome parse it.
     x.responseType = 'json';
-    x.onload = function () {
-        var response = x.response;
 
+    x.onload = function () {
+        clearTimeout(timer);
+        var response = x.response;
         if (!response || !response.views) {
-            console.log("error fetching");
+            renderLogin();
             return;
         }
 
         buildHTMLfromViews(response.views);
     };
-    x.onerror = function(e){
-        renderStatus('Not authenticated ... please <a class="login" href="https://jenkins.whatclinic.net" target="_blank">login</a> manually first');
+
+
+    x.onerror = function (e) {
+        renderLogin()
     }
     console.log("about to send")
     x.send();
 }
 
+function renderLogin() {
+    renderStatus('Not authenticated ... please <a class="login" href="https://jenkins.whatclinic.net" target="_blank">login</a> manually first');
+}
 
 document.addEventListener('DOMContentLoaded', function () {
 
